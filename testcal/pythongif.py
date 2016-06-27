3e2c9af7dad5d2d49c856f1143e91ea23f48f884
Jess Whyte <jess.whyte@gmail.com>
2016-06-27 14:20:20 -0400

    testcal testing fun

---
+++ testcal/pythongif.py
@@ -0,0 +1,14 @@
+import webbrowser
+import time
+
+##dictate specific webbrowser
+#b = webbrowser.get('lynx')
+#b.open('http://docs.python.org/lib/module-webbrowser.html')
+
+url='testgif.html'
+#browser=webbrowser.get("chrome")
+
+webbrowser.open(url)
+time.sleep(3) #delay for 5 seconds
+webbrowser.open('http://studio307.github.io/')
+
